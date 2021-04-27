ab2ea7c52c4cb027d5a57fc78fb94a5d78fe3e61
Pedro Costa <pedrorebelocosta@gmail.com>
2021-04-27 17:14:24 +0100

    feat(jwt-auth-middleware) - add middleware to verify incoming requests

--- index.php
+++ index.php
@@ -10,6 +10,22 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $app = AppFactory::create();
+
+// Add JWT Middleware to authenticate requests
+$app->add(new Tuupola\Middleware\JwtAuthentication([
+    "ignore" => ["/auth"],
+    "secret" => $_ENV['SECRET_KEY'],
+    "rules" => [
+        new Tuupola\Middleware\JwtAuthentication\RequestPathRule([
+            "path" => "/occurrence",
+            "ignore" => []
+        ]),
+        new Tuupola\Middleware\JwtAuthentication\RequestMethodRule([
+            "ignore" => ["OPTIONS", "GET"]
+        ])
+    ]
+]));
+
 $app->addBodyParsingMiddleware();
 
 $app->get('/', function (Request $request, Response $response, $args) {
