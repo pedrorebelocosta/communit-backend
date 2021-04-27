a52432d6bf1a7d8926093ee761ee8909cb79d191
Pedro Costa <pedrorebelocosta@gmail.com>
2021-04-27 23:47:45 +0100

    refactor auth controller/service - if statements optimization

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,40 +1,43 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Firebase\JWT\JWT;
 
 $app->post('/auth', function (Request $request, Response $response, $args) {
-    require_once("db.php");
-    $body = $request->getParsedBody();
-    if (isset($body)) {
-        if (!(isset($body['email']) && isset($body['password']))) {
-            return;
-        }
-        $foundUser = $db->user()("email = ?", $body['email'])->fetch();
-        if (password_verify($body['password'], $foundUser['password'])) {
-            // Returning a JWT to client
-            $payload = array(
-                "iss" => "localhost:1337",
-                "aud" => "com.cpe.communit:app",
-                "iat" => time(),
-                "exp" => strtotime("+1 year"),
-                "email" => $foundUser['email']
-            );
-            $responseBody = json_encode([
-                "authenticated" => true,
-                "auth_token"=> JWT::encode($payload, $_ENV['SECRET_KEY'])
-            ]);
-            $response->getBody()->write($responseBody);
-            return $response;
-        } else {
-            $responseBody = json_encode([
-                "authenticated" => false,
-                "status"=> "Username and/or password incorrect",
-            ]);
-            $response->getBody()->write($responseBody);
-            return $response;
-        }
-    }
-});
+	require_once("db.php");
+	$body = $request->getParsedBody();
+	if (!isset($body)) return $response->withStatus(400);
+	if (!(isset($body['email']) && isset($body['password']))) {
+		return $response->withStatus(400);
+	}
+	$foundUser = $db->user()("email = ?", $body['email'])->fetch();
+	if (!password_verify($body['password'], $foundUser['password'])) {
+		$responseBody = json_encode([
+			"authenticated" => false,
+			"status" => "Username and/or password incorrect",
+		]);
+		$response->getBody()->write($responseBody);
+		return $response;
+	}
 
-?>
+	/*
+		After all verifications, everything seems to be fine
+		Returning a JWT to client
+	*/
+	$payload = array(
+		"iss" => "localhost:1337",
+		"aud" => "com.cpe.communit:app",
+		"iat" => time(),
+		"exp" => strtotime("+1 year"),
+		"email" => $foundUser["email"],
+		"first_name" => $foundUser["firstname"],
+		"last_name" => $foundUser["lastname"]
+	);
+	$responseBody = json_encode([
+		"authenticated" => true,
+		"auth_token" => JWT::encode($payload, $_ENV['SECRET_KEY'])
+	]);
+	$response->getBody()->write($responseBody);
+	return $response;
+});
